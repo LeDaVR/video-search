7993fe090abb711577912923a024b9bf48c5b9aa
LeDaVR <42551056+LeDaVR@users.noreply.github.com>
2022-12-25 22:39:07 -0500

    port

--- flask-youtube/app.py
+++ flask-youtube/app.py
@@ -84,4 +84,4 @@ def watch():
     return render_template('youtube.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
